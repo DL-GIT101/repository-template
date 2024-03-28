847b5cbc2127f0f849ec3dfb4ca02925978f1ae0
donLhenard <90548800+DL-GIT101@users.noreply.github.com>
2024-03-28 11:20:37 +0800

    Update webpack.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const ESLintPlugin = require('eslint-webpack-plugin');
 
  module.exports = {
    entry: {
@@ -17,6 +18,51 @@ const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
         filename: "[name].css",
         chunkFilename: "[id].css",
       }),
+      new ESLintPlugin(),
+   ],
+   module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [
+      new CssMinimizerPlugin(),
+    ],
+  },
+   output: {
+     filename: '[name].bundle.js',
+     path: path.resolve(__dirname, 'dist'),
+     clean: true,
+   },
+ };const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+ module.exports = {
+   entry: {
+     app: './src/index.js',
+   },
+   plugins: [
+     new HtmlWebpackPlugin({
+       template: './src/index.html',
+       filename: 'index.html',
+       inject: 'body',
+     }),
+     new MiniCssExtractPlugin({
+        filename: "[name].css",
+        chunkFilename: "[id].css",
+      }),
+      new ESLintPlugin(),
    ],
    module: {
     rules: [
